2d432f6565e771e5b0839174b428a498195d0613
haoyinag <haoyinag0@hotmail.com>
2020-04-23 18:27:57 +0800

    配置lv1.0.1

--- config/config.ts
+++ config/config.ts
@@ -1,94 +1,80 @@
 import { defineConfig } from 'umi';
 
 /** 把路由文件放在src下是为了方便按模块拆分组装路由表，而且方便通过请求配置access */
-import routes from '../src/router'
+import routes from '../src/router';
 
 export default defineConfig({
-    title: 'site.title',
-    hash: true,
-    /** 开启dva */
-    dva: {
-        hmr: true, // 热更新
-        immer: true, // 是否启用 immer 以方便修改 reducer
-        skipModelValidate: false, // 是否跳过 model 验证
-        extraModels: [], // 配置额外到 dva model 
-    },
-    /** 启用antd */
-    antd: {
-        dark: false, // 开启暗色主题
-        compact: false, // 开启紧凑主题
-    },
-    /** 国际化--构建式配置，src/locals是约定式配置*/
-    // locale: { 
-    //      title:true, // 标题国际化.在项目中配置的 title 及路由中的 title 可直接使用国际化 key，自动被转成对应语言的文案
-    //     default: 'zh-CN', // 默认语言，当检测不到具体语言时，展示 default 中指定的语言
-    //     // default true, when it is true, will use `navigator.language` overwrite default
-    //     antd: true, // 开启后，支持 antd 国际化
-    //     baseNavigator: true, // 开启浏览器语言检测
-    //     baseSeparator: '-', // 国家（lang） 与 语言（language） 之间的分割符
-    //      ...
-    // },
-    /** 浏览器兼容 */
-    targets: {
-        ie: 11,
-    },
-    /** 启用umi的layout插件
-     *  如果不开启，layout需要自己实现，
-     *  并且src\app.ts的相关如rightRender、logout等会报错
-     *  开启之后，会自动读取当前的·routes·配置
+  title: 'site.title',
+  hash: true,
+  base: './',
+  history: { type: 'hash' },
+  publicPath: '/',
+  // mock: false, // mock开关
+  /** 开启dva */
+  dva: {
+    hmr: true, // 热更新
+    immer: true, // 是否启用 immer 以方便修改 reducer
+    skipModelValidate: false, // 是否跳过 model 验证
+    extraModels: [], // 配置额外到 dva model
+  },
+  /** 启用antd */
+  antd: {
+    dark: false, // 开启暗色主题
+    compact: false, // 开启紧凑主题
+  },
+  /** 国际化--构建式配置，src/locals是约定式配置*/
+  // locale: {
+  //      title:true, // 标题国际化.在项目中配置的 title 及路由中的 title 可直接使用国际化 key，自动被转成对应语言的文案
+  //     default: 'zh-CN', // 默认语言，当检测不到具体语言时，展示 default 中指定的语言
+  //     // default true, when it is true, will use `navigator.language` overwrite default
+  //     antd: true, // 开启后，支持 antd 国际化
+  //     baseNavigator: true, // 开启浏览器语言检测
+  //     baseSeparator: '-', // 国家（lang） 与 语言（language） 之间的分割符
+  //      ...
+  // },
+  /** 浏览器兼容 */
+  targets: {
+    ie: 11,
+  },
+  /** 启用umi的layout插件
+   *  如果不开启，layout需要自己实现，
+   *  并且src\app.ts的相关如rightRender、logout等会报错
+   *  开启之后，会自动读取当前的·routes·配置
+   */
+  layout: {
+    name: 'UmiPro', // 侧边栏头部产品名，默认值为包名
+    // logo: '', // 产品 Logo
+    theme: 'pro', // 指定 Layout 主题, tech 仅在蚂蚁内部框架 Bigfish 中生效
+    // locale: true, //
+    /** 是否开始国际化配置。开启后路由里配置的菜单名会被当作菜单名国际化的 key，
+     * 插件会去 locales 文件中查找 menu.[key]对应的文案，默认值为改 key。
+     * 该功能需要配置 @umijs/plugin-locale 使用 */
+    locale: true,
+  },
+  routes,
+  /** qiankun
+     * ✔︎ 基于 qiankun
+        ✔︎ 支持主应用和子应用都用 umi
+        ✔︎ 支持主子应用 browser、hash 等多种 history 模式
+        ✔︎ 父子应用通讯
+        ✔︎ 子应用运行时配置自定义 bootstrap()、mount() 和 unmount()
+        ✔︎ 主应用、子应用联调
      */
-    layout: {
-        name: 'Ant Design', // 产品名，默认值为包名
-        // logo: '', // 产品 Logo
-        theme: 'pro', // 指定 Layout 主题, tech 仅在蚂蚁内部框架 Bigfish 中生效
-        // locale: true, // 
-        /** 是否开始国际化配置。开启后路由里配置的菜单名会被当作菜单名国际化的 key，
-         * 插件会去 locales 文件中查找 menu.[key]对应的文案，默认值为改 key。
-         * 该功能需要配置 @umijs/plugin-locale 使用 */
-        locale: true,
-    },
-    routes,
-    //  [
-    //     { path: '/', component: '@/pages/index' },
-    //     {
-    //         path: '/admin',
-    //         name: 'admin',
-    //         icon: 'crown',
-    //         /** access--当 Layout 插件配合 @umijs/plugin-access 插件使用时生效
-    //          *  权限插件会将用户在这里配置的 access 字符串与当前用户所有权限做匹配，
-    //          *  如果找到相同的项，并当该权限的值为 false，则当用户访问该路由时，默认展示 403 页面
-    //          */
-    //         access: 'canAdmin',
-    //         component: './Admin',
-    //         layout: {
-    //             // hideNav: true, // 是否当前路由隐藏导航头，默认不隐藏
-    //             // hideMenu: true, // 是否当前路由隐藏左侧菜单，默认不隐藏
-    //         },
-    //         routes: [
-    //             {
-    //                 path: '/admin/sub-page',
-    //                 name: 'sub-page',
-    //                 icon: 'smile',
-    //                 component: './Welcome',
-    //             },
-    //         ],
-    //     },
-    // ],
-
-    /** 开启按需加载 */
-    dynamicImport: {
-        loading: '@/Loading', // 全局loading组件
-    },
-    /** 主题配置 */
-    theme: {
-        // ...darkTheme,
-        // 'primary-color': defaultSettings.primaryColor,
-    },
-    ignoreMomentLocale: true,
-    nodeModulesTransform: {
-        type: 'none',
-    },
-    manifest: {
-        basePath: '/',
-    },
+  // qiankun:{},
+  /** 开启按需加载 */
+  dynamicImport: {
+    loading: '@/Loading', // 全局loading组件
+  },
+  /** 主题配置 */
+  theme: {
+    // ...darkTheme,
+    // 'primary-color': defaultSettings.primaryColor,
+  },
+  ignoreMomentLocale: true,
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  manifest: {
+    basePath: '/',
+  },
 });
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,61 @@
-
+/** 运行时配置文件，可以在这里扩展运行时的能力，比如修改路由、修改 render 方法等。 */
+import { history } from 'umi';
 /** layout有操作都会执行 */
 export const layout = {
-    /** 发生错误后的回调（可做一些错误日志上报，打点等） */
-    onError: (e: any) => {
-        console.log(e);
-    },
-    /** 发生错误后展示的组件 */
-    ErrorComponent: (e: any) => {
-        console.log(e);
-    },
+  /** 发生错误后的回调（可做一些错误日志上报，打点等） */
+  onError: (e: any) => {
+    console.log(e);
+  },
+  /** 发生错误后展示的组件 */
+  ErrorComponent: (e: any) => {
+    console.log(e);
+  },
 
-    /** 点击退出登录的处理逻辑，默认不做处理 */
-    logout: (e: any) => {
-        console.log(e);
-    },
-    /** 包括侧边栏开合 */
-    rightRender: (initInfo: any) => {
-        console.log(initInfo);
+  /** 点击退出登录的处理逻辑，默认不做处理 */
+  logout: (e: any) => {
+    console.log(e);
+  },
+  /** 顶部栏开合 */
+  rightRender: (initInfo: any) => {
+    console.log(initInfo);
 
-        return 'hahah';
-    }, // return string || ReactNode;
+    return 'hahah';
+  }, // return string || ReactNode;
 };
+
+// export function patchRoutes({ routes }) {
+//     merge(routes, extraRoutes);
+// }
+
+/** 在初始加载和路由切换时做一些事情--比如用于做埋点统计/比如用于设置标题 */
+export function onRouteChange({
+  matchedRoutes,
+  location,
+  routes,
+  action,
+}: {
+  matchedRoutes: any;
+  location: any;
+  routes: any;
+  action: any;
+}) {
+  // bacon(location.pathname);
+  if (matchedRoutes.length) {
+    document.title = matchedRoutes[matchedRoutes.length - 1].route.title || '';
+  }
+}
+
+/** render覆写 render，会直接阻断所有的运行时 */
+// export function render(oldRender: any) {
+//     console.log(oldRender);
+//     // fetch('/api/auth').then((auth: any) => {
+//     //     if (auth.isLogin) { oldRender() }
+//     //     else { history.push('/login'); }
+//     // });
+// }
+
+/** 修改交给 react-dom 渲染时的根组件 */
+// export function rootContainer(container,args:{routes，全量路由配置,plugin，运行时插件机制，history，history 实例}) {
+// 比如用于在外面包一个 Provider
+//     return React.createElement(ThemeProvider, null, container);
+//   }
