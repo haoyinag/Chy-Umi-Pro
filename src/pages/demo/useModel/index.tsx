4848175587f0a7f12902cc68967c6066e62f0077
haoyinag <haoyinag0@hotmail.com>
2020-04-24 18:11:25 +0800

    feat：新增model使用demo

---
+++ src/pages/demo/useModel/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react';
+import {
+  UseModelDemoState,
+  ConnectProps,
+  Loading,
+  connect,
+  useDispatch,
+} from 'umi';
+
+import { Button } from 'antd';
+
+interface PageProps extends ConnectProps {
+  useModel: UseModelDemoState;
+  loading: boolean;
+}
+
+const IndexPage: FC<PageProps> = ({ useModel }) => {
+  const { name } = useModel;
+  const dispatch = useDispatch();
+
+  const onClick = (num: number) => {
+    dispatch({
+      type: 'useModel/query',
+      payload:
+        num === 1 ? { name: '哈哈，我改变了useModel' } : { name: 'useModel' },
+    });
+  };
+  return (
+    <div>
+      Hello {name}
+      <br />
+      <Button type="primary" onClick={() => onClick(1)}>
+        dispatch
+      </Button>
+      <Button type="primary" onClick={() => onClick(2)}>
+        重置
+      </Button>
+    </div>
+  );
+};
+
+export default connect(
+  ({
+    useModel,
+    loading,
+  }: {
+    useModel: UseModelDemoState;
+    loading: Loading;
+  }) => ({
+    useModel,
+    loading: loading.models.useModel,
+  }),
+)(IndexPage);
