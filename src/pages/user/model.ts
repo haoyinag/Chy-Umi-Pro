ef976cbcaa4e5e0fa85980f79504b9f7ed3c16b5
haoyinag <haoyinag0@hotmail.com>
2020-04-26 16:28:56 +0800

    feat：新增登录的model

---
+++ src/pages/user/model.ts
@@ -0,0 +1,61 @@
+import { Effect, ImmerReducer, Subscription } from 'umi';
+
+export interface LoginState {
+  code: string;
+}
+
+export interface LoginType {
+  namespace: 'login';
+  state: LoginState;
+  effects: {
+    queryCode: Effect;
+  };
+  reducers: {
+    // getCode: Reducer<LoginState>;
+    // 启用 immer 之后
+    getCode: ImmerReducer<LoginState>;
+  };
+  subscriptions: { setup: Subscription };
+}
+
+const Login: LoginType = {
+  namespace: 'login',
+  state: {
+    code: '',
+  },
+  effects: {
+    *queryCode({ payload }, { call, put }: { call: any; put: any }): any {
+      yield put({
+        type: 'getCode',
+        payload: payload,
+      });
+    },
+  },
+  reducers: {
+    // getCode(state, action) {
+    //     return {
+    //         ...state,
+    //         ...action.payload,
+    //     };
+    // },
+    // 启用 immer 之后
+    getCode(state: any, action: any) {
+      state.code = action.payload;
+    },
+  },
+  subscriptions: {
+    setup({ dispatch, history }: { dispatch: any; history: any }) {
+      return history.listen(({ pathname }: { pathname: any }) => {
+        console.log(pathname);
+
+        // if (pathname === '/') {
+        //   dispatch({
+        //     type: 'queryCode',
+        //   });
+        // }
+      });
+    },
+  },
+};
+
+export default Login;
